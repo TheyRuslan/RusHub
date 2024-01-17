29dbc00571d4961c5187586ad755bbee29dd48ac
TheyRuslan <95719600+TheyRuslan@users.noreply.github.com>
2024-01-17 20:28:39 +0100

    Update l0adstring.lua

--- l0adstring.lua
+++ l0adstring.lua
@@ -2,6 +2,97 @@ wait(1)
 local Camera = game:GetService("Workspace").CurrentCamera
 local CharcaterMiddle = game:GetService("Workspace").Ignore.LocalCharacter.Middle
 --[[
+
+local originalTerrainDecoration = gethiddenproperty(game.Workspace.Terrain, "Decoration")
+local originalGlobalShadows = gethiddenproperty(game:GetService("Lighting"), "GlobalShadows")
+
+-- Hook para modificar la propiedad "Decoration" del terreno
+local terrainHook = hookmetamethod(game, "__newindex", newcclosure(function(...)
+    local self, k, v = ...
+    if not checkcaller() and self == game.Workspace.Terrain and k == "Decoration" then
+        return false
+    end
+    return terrainHook(...)
+end))
+
+-- Hook para modificar la propiedad "GlobalShadows" de Lighting
+local shadowsHook = hookmetamethod(game, "__newindex", newcclosure(function(...)
+    local self, k, v = ...
+    if not checkcaller() and self == game:GetService("Lighting") and k == "GlobalShadows" then
+        return false
+    end
+    return shadowsHook(...)
+end))
+
+local originalEqualizerSoundEffect = game:GetService("SoundService").PlayerHitHeadshot.EqualizerSoundEffect
+local originalSoundId = game:GetService("SoundService").PlayerHitHeadshot.SoundId
+
+-- Hook para modificar el sonido EqualizerSoundEffect
+local soundHook = hookmetamethod(game, "__newindex", newcclosure(function(...)
+    local self, k, v = ...
+    if not checkcaller() and self == originalEqualizerSoundEffect and k == "HighGain" then
+        return -1.5
+    end
+    return soundHook(...)
+end))
+
+-- Hook para modificar el SoundId
+local soundIdHook = hookmetamethod(game, "__newindex", newcclosure(function(...)
+    local self, k, v = ...
+    if not checkcaller() and self == originalSoundId and k == "SoundId" then
+        return "rbxassetid://8726881116"
+    end
+    return soundIdHook(...)
+end))
+
+local antihideprop
+
+antihideprop = hookmetamethod(game.Workspace.Terrain, "__newindex", newcclosure(function(...)
+    local self, k, v = ...
+    if not checkcaller() and k == "Decoration" then
+        print("Modificación bloqueada. Manteniendo el valor en true.")
+        return true  -- Mantén el valor como true
+    end
+    return antihideprop(...)
+end))
+
+
+local antihideshadows
+
+antihideshadows = hookmetamethod(game:GetService("Lighting"), "__newindex", newcclosure(function(...)
+    local self, k, v = ...
+    if not checkcaller() and k == "GlobalShadows" then
+        print("Modificación bloqueada. Manteniendo el valor en true.")
+        return true  -- Mantén el valor como true
+    end
+    return antihideshadows(...)
+end))
+
+local antiSoundChanges
+
+antiSoundChanges = hookmetamethod(game:GetService("SoundService").PlayerHitHeadshot, "__newindex", newcclosure(function(...)
+    local self, k, v = ...
+    
+    if not checkcaller() and (k == "EqualizerSoundEffect" or k == "SoundId") then
+        print("Modificación bloqueada. Manteniendo los valores originales.")
+        return true  -- Mantén el valor original
+    end
+    
+    return antiSoundChanges(...)
+end))
+
+
+local defaultBrightness = game:GetService("Lighting").Brightness
+
+local hookBrightness = hookmetamethod(game, "__newindex", newcclosure(function(...)
+    local self, property, value = ...
+    if self:IsA("Lighting") and property == "Brightness" then
+        warn("¡Alguien está jugando con el brillo!")
+        return hookBrightness(self, property, defaultBrightness) -- Establecer el brillo actual como predeterminado
+    end
+    return hookBrightness(...)
+end))
+
 game:GetService("SoundService").PlayerHitHeadshot.EqualizerSoundEffect.HighGain = -1.5
 game:GetService("SoundService").PlayerHitHeadshot.SoundId = "rbxassetid://8726881116"
 
@@ -11,53 +102,51 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Dustin21335/Full-brig
 ]]
 local antihitbox
 antihitbox = hookmetamethod(game, "__newindex", newcclosure(function(...)
-local self, k = ...
-if not checkcaller() and k == "Size" and self.Name == "Head" then
-  return Vector3.new(1.672248125076294, 0.835624098777771, 0.835624098777771)
-end
-return antihitbox(...)
+    local self, k = ...
+    if not checkcaller() and k == "Size" and self.Name == "Head" then
+        return Vector3.new(1.672248125076294, 0.835624098777771, 0.835624098777771)
+    end
+    return antihitbox(...)
 end))
 
 local antihitbox2
 antihitbox2 = hookmetamethod(game, "__index", newcclosure(function(...)
-local self, k = ...
-if not checkcaller() and k == "Size" and self.Name == "Torso" then
-  return 0.6530659198760986, 2.220424175262451, 1.4367451667785645
-end
-return antihitbox2(...)
+    local self, k = ...
+    if not checkcaller() and k == "Size" and self.Name == "Torso" then
+        return 0.6530659198760986, 2.220424175262451, 1.4367451667785645
+    end
+    return antihitbox2(...)
 end))
 
-_G.ruslan = true  -- Global variable to control loop execution
+_G.ruslan = true
 
 while _G.ruslan do
     local randNumHead = math.random(1, 100)
     local randNumTorso = math.random(1, 100)
     local xdHead, xdTorso
 
-    -- Head probabilities
     if randNumHead <= 50 then
-        xdHead = 4.5
+        xdHead = 3.8
     elseif randNumHead <= 80 then
-        xdHead = 5.3
+        xdHead = 4.5
     elseif randNumHead <= 90 then
-        xdHead = 6
+        xdHead = 5.2
     elseif randNumHead <= 95 then
-        xdHead = 7.5
+        xdHead = 5.7
     else
-        xdHead = 7.8
+        xdHead = 6
     end
 
-    -- Torso probabilities
     if randNumTorso <= 20 then
-        xdTorso = 6
+        xdTorso = 4.7
     elseif randNumTorso <= 50 then
-        xdTorso = 7.4
+        xdTorso = 5.2
     elseif randNumTorso <= 70 then
-        xdTorso = 8
+        xdTorso = 5.8
     elseif randNumTorso <= 88 then
-        xdTorso = 9
+        xdTorso = 6
     else
-        xdTorso = 9.4
+        xdTorso = 7
     end
 
     local HitboxExpanderHead = { HitBX = xdHead, HitBY = xdHead, HitBZ = xdHead }
@@ -70,6 +159,7 @@ while _G.ruslan do
                 i.Head.CanCollide = false
                 i.Head.Color = Color3.fromRGB(144, 66, 245)
                 i.Head.Material = "ForceField"
+                i.Head.Transparency = 
             end
             if i:FindFirstChild("Torso") then
                 i.Torso.Size = Vector3.new(HitboxExpanderTorso.HitBX, HitboxExpanderTorso.HitBY, HitboxExpanderTorso.HitBZ)
@@ -83,6 +173,5 @@ while _G.ruslan do
     game.ReplicatedStorage.Player.Head.Size = Vector3.new(HitboxExpanderHead.HitBX, HitboxExpanderHead.HitBY, HitboxExpanderHead.HitBZ)
     game.ReplicatedStorage.Player.Torso.Size = Vector3.new(HitboxExpanderTorso.HitBX, HitboxExpanderTorso.HitBY, HitboxExpanderTorso.HitBZ)
 
-    wait(0.65)
+    wait(math.random() * 0.4 + 0.4)
 end
-print("yo")
