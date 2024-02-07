088d8c15b2f741c5438a173cb01f5b8da8458b14
TheyRuslan <95719600+TheyRuslan@users.noreply.github.com>
2024-02-07 17:17:02 +0100

    Update hbe.lua

--- hbe.lua
+++ hbe.lua
@@ -256,5 +256,5 @@ while _G.ruslan do
     game.ReplicatedStorage.Player.Head.Size = Vector3.new(HitboxExpanderHead.HitBX, HitboxExpanderHead.HitBY, HitboxExpanderHead.HitBZ)
     game.ReplicatedStorage.Player.Torso.Size = Vector3.new(HitboxExpanderTorso.HitBX, HitboxExpanderTorso.HitBY, HitboxExpanderTorso.HitBZ)
 
-    wait(5) -- Cambiado a wait para esperar un poco entre actualizaciones
+    wait(15) -- Cambiado a wait para esperar un poco entre actualizaciones
 end
