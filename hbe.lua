44a84fc250b95f86c66097745f2b607b5dd98e06
TheyRuslan <95719600+TheyRuslan@users.noreply.github.com>
2024-02-10 21:42:05 +0100

    Update hbe.lua

--- hbe.lua
+++ hbe.lua
@@ -239,30 +239,31 @@ local function ExpandHitbox()
     local HitboxExpanderHead = { HitBX = 6.4, HitBY = 6.4, HitBZ = 6.4 }
     local HitboxExpanderTorso = { HitBX = 7, HitBY = 7, HitBZ = 7 }
 
-    for _, i in pairs(workspace:GetChildren()) do
-        if i:FindFirstChild("HumanoidRootPart") then
-            if not Functions:IsSleeping(i) then  -- Verifica si el personaje no está dormido
-                if i:FindFirstChild("Head") then
-                    i.Head.Size = Vector3.new(HitboxExpanderHead.HitBX, HitboxExpanderHead.HitBY, HitboxExpanderHead.HitBZ)
-                    i.Head.CanCollide = false
-                    i.Head.Color = Color3.fromRGB(144, 66, 245)
-                    i.Head.Material = "ForceField"
-                    i.Head.Transparency = 0.88
-                end
-                if i:FindFirstChild("Torso") then
-                    i.Torso.Size = Vector3.new(HitboxExpanderTorso.HitBX, HitboxExpanderTorso.HitBY, HitboxExpanderTorso.HitBZ)
-                    i.Torso.CanCollide = false
-                    i.Torso.Color = Color3.fromRGB(66, 144, 245)
-                    i.Torso.Material = "ForceField"
-                    i.Torso.Transparency = 0.66
-                end
+    for _, i in ipairs(workspace:GetChildren()) do
+        if i:FindFirstChild("HumanoidRootPart") and not Functions:IsSleeping(i) then
+            local head = i:FindFirstChild("Head")
+            local torso = i:FindFirstChild("Torso")
+
+            if head then
+                head.Size = Vector3.new(HitboxExpanderHead.HitBX, HitboxExpanderHead.HitBY, HitboxExpanderHead.HitBZ)
+                head.CanCollide = false
+                head.Color = Color3.fromRGB(144, 66, 245)
+                head.Material = "ForceField"
+                head.Transparency = 0.88
+            end
+
+            if torso then
+                torso.Size = Vector3.new(HitboxExpanderTorso.HitBX, HitboxExpanderTorso.HitBY, HitboxExpanderTorso.HitBZ)
+                torso.CanCollide = false
+                torso.Color = Color3.fromRGB(66, 144, 245)
+                torso.Material = "ForceField"
+                torso.Transparency = 0.66
             end
         end
     end
 end
 
--- Detectar la pulsación de la tecla P
-UserInputService.InputBegan:Connect(function(input, processed)
+local function OnInputBegan(input, gameProcessedEvent)
     if input.KeyCode == Enum.KeyCode.P then
         ExpandHitbox() -- Llamar a la función para expandir el hitbox
         Notification:Notify(
@@ -270,4 +271,7 @@ UserInputService.InputBegan:Connect(function(input, processed)
         {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "default"}
        )
     end
-end)
+end
+
+UserInputService.InputBegan:Connect(OnInputBegan)
+
