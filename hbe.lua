81db2d8e369f3e03c04e582c9b0835accaccf069
TheyRuslan <95719600+TheyRuslan@users.noreply.github.com>
2024-02-11 15:50:01 +0100

    Update hbe.lua

--- hbe.lua
+++ hbe.lua
@@ -29,20 +29,15 @@ antihitbox2 = hookmetamethod(game, "__index", newcclosure(function(...)
     return antihitbox2(...)
 end))
 
-
 local Functions = {}
 local Esp = {
     Settings = {
-        Boxes = false,
-        BoxesColor = Color3.fromRGB(144, 66, 245),
-        Sleeping = false,
-        SleepingColor = Color3.fromRGB(255, 255, 255),
-        Distances = false,
-        DistanceColor = Color3.fromRGB(255, 255, 255),
-        Armour = false,
-        ArmourColor = Color3.fromRGB(255, 255, 255),
         Tool = false,
         ToolColor = Color3.fromRGB(255, 255, 255),
+        Sleeping = false,
+        SleepingColor = Color3.fromRGB(255, 255, 255),
+        Armour = false,
+        ArmourColor = Color3.fromRGB(255, 255, 255),
         ViewAngle = true,
         ViewAngleColor = Color3.fromRGB(144, 66, 245),
         ViewAngleThickness = 1,
@@ -56,21 +51,14 @@ local Esp = {
         MinTextSize = 11
     },
     Drawings = {},
-    Connections = {},
     Players = {}
-}
-
-local Fonts = {
-    ["UI"] = 0,
-    ["System"] = 1,
-    ["Plex"] = 2,
-    ["Monospace"] = 3
 }
 
 local cache = {}
 
 -- Función para limpiar el cache
 local MAX_CACHE_SIZE = 100
+local CLEAN_INTERVAL = 60  -- Limpiar el cache cada 60 segundos
 
 local function ClearCache()
     while #cache > MAX_CACHE_SIZE do
@@ -79,159 +67,86 @@ local function ClearCache()
 end
 
 -- Llamar a la función de limpieza de cache periódicamente
-local CLEAN_INTERVAL = 60  -- Limpiar el cache cada 60 segundos
-
-local function PeriodicCacheCleanup()
+spawn(function()
     while true do
         wait(CLEAN_INTERVAL)
         ClearCache()
     end
-end
-
--- Iniciar la función de limpieza de cache en un hilo aparte
-spawn(PeriodicCacheCleanup)
+end)
 
 -- Functions
 function Functions:IsSleeping(Model)
-    if Model and Model:FindFirstChild("AnimationController") and Model.AnimationController:FindFirstChild("Animator") then
-        for i, v in pairs(Model.AnimationController.Animator:GetPlayingAnimationTracks()) do
-            if v.Animation.AnimationId == "rbxassetid://13280887764" then
+    if Model and Model:FindFirstChild("AnimationController") then
+        for _, v in pairs(Model.AnimationController:GetDescendants()) do
+            if v:IsA("AnimationTrack") and v.Animation.AnimationId == "rbxassetid://13280887764" then
                 return true
-            else
-                return false
             end
         end
     end
+    return false
 end
 
-function Functions:Draw(Type, Properties)
-    if not Type and not Properties then
-        return
-    end
-    local drawing = Drawing.new(Type)
-    for i, v in pairs(Properties) do
-        drawing[i] = v
-    end
-    table.insert(Esp.Drawings, drawing)
-    return drawing
+function Esp:CreateEsp(Player)
+    local ViewAngle = Drawing.new("Line")
+    ViewAngle.Thickness = Esp.Settings.ViewAngleThickness
+    ViewAngle.Transparency = Esp.Settings.ViewAngleTransparency
+    ViewAngle.Visible = false
+    ViewAngle.Color = Esp.Settings.ViewAngleColor
+    self.Drawings[Player] = { ViewAngle = ViewAngle }
 end
 
-function Esp:CreateEsp(PlayerTable)
-    if not PlayerTable then
-        return
-    end
-    local drawings = {}
-    drawings.Box = Functions:Draw("Square", {Transparency = 0.35, Color = Esp.Settings.BoxesColor, Visible = false, Visible = false})
-    drawings.Sleeping = Functions:Draw("Text", {Text = "Nil", Font = Esp.Settings.TextFont, Size = Esp.Settings.TextSize, Center = true, Outline = Esp.Settings.TextOutline, Color = Esp.Settings.SleepingColor, ZIndex = 2, Visible = false})
-    drawings.Distance = Functions:Draw("Text", {Text = "Nil", Font = Esp.Settings.TextFont, Size = Esp.Settings.TextSize, Center = true, Outline = Esp.Settings.TextOutline, Color = Esp.Settings.SleepingColor, ZIndex = 2, Visible = false})
-    drawings.Armour = Functions:Draw("Text", {Text = "None", Font = Esp.Settings.TextFont, Size = Esp.Settings.TextSize, Center = false, Outline = Esp.Settings.TextOutline, Color = Esp.Settings.ArmourColor, ZIndex = 2, Visible = false})
-    drawings.ViewAngle = Functions:Draw("Line", {Thickness = Esp.Settings.ViewAngleThickness, Transparency = Esp.Settings.ViewAngleTransparrency, Color = Esp.Settings.ViewAngleColor, ZIndex = 2, Visible = false})
-    drawings.PlayerTable = PlayerTable
-    Esp.Players[PlayerTable.model] = drawings
-end
-
-function Esp:RemoveEsp(PlayerTable)
-    if not PlayerTable and PlayerTable.model ~= nil then
-        return
-    end
-    esp = Esp.Players[PlayerTable.model]
-    if not esp then
-        return
-    end
-    for i, v in pairs(esp) do
-        if not type(v) == "table" then
-            v:Remove()
+function Esp:RemoveEsp(Player)
+    local drawings = self.Drawings[Player]
+    if not drawings then return end
+    for _, drawing in pairs(drawings) do
+        if type(drawing) == "table" then
+            drawing:Remove()
         end
     end
-    Esp.Players[PlayerTable.model] = nil
+    self.Drawings[Player] = nil
 end
 
 function Esp:UpdateEsp()
-    for i,v in pairs(Esp.Players) do
-        local Character = i
-        local Position,OnScreen = Camera:WorldToViewportPoint(Character:GetPivot().Position);
-        local scale = 1 / (Position.Z * math.tan(math.rad(Camera.FieldOfView * 0.5)) * 2) * 100;
-        local w,h = math.floor(32 * scale), math.floor(60 * scale);
-        local x,y = math.floor(Position.X), math.floor(Position.Y);
-        local Distance = (CharcaterMiddle:GetPivot().Position-Character:GetPivot().Position).Magnitude
-        local BoxPosX,BoxPosY = math.floor(x - w * 0.5),math.floor(y - h * 0.5)
-        local offsetCFrame = CFrame.new(0, 0, -4)
-        local sleeping = Functions:IsSleeping(Character)
+    for Player, drawings in pairs(self.Drawings) do
+        local Character = Player.Character
         if Character and Character:FindFirstChild("HumanoidRootPart") and Character:FindFirstChild("Head") then
-            local TeamTag = Character.Head.Teamtag.Enabled
-            if OnScreen == true and Esp.Settings.Boxes == true and Distance <= Esp.Settings.RenderDistance then
-                if Esp.Settings.TeamCheck == true and TeamTag == false then
-                    v.Box.Visible = true
-                elseif Esp.Settings.TeamCheck == true and TeamTag == true then
-                    v.Box.Visible = false
-                else
-                    v.Box.Visible = true
-                end
-                if Esp.Settings.TargetSleepers == true and sleeping == true then
-                    v.Box.Visible = false
-                end
-                v.Box.Position = Vector2.new(BoxPosX, BoxPosY)
-                v.Box.Size = Vector2.new(w, h)
-                v.Box.Color = Esp.Settings.BoxesColor
+            local Position, OnScreen = Camera:WorldToViewportPoint(Character.Head.Position)
+            local Distance = (CharcaterMiddle:GetPivot().Position - Character:GetPivot().Position).Magnitude
+            local sleeping = Functions:IsSleeping(Character)
+            if OnScreen and Esp.Settings.ViewAngle and Distance <= Esp.Settings.RenderDistance then
+                drawings.ViewAngle.Visible = true
+                local headPos = Camera:WorldToViewportPoint(Character.Head.Position)
+                local dir = Character.Head.CFrame:ToWorldSpace(CFrame.new(0, 0, -4))
+                local dirPos = Camera:WorldToViewportPoint(dir.Position)
+                drawings.ViewAngle.From = Vector2.new(headPos.X, headPos.Y)
+                drawings.ViewAngle.To = Vector2.new(dirPos.X, dirPos.Y)
             else
-                v.Box.Visible = false
-            end
-            if OnScreen == true and Esp.Settings.ViewAngle == true and Distance <= Esp.Settings.RenderDistance then
-                if Esp.Settings.TeamCheck == true and TeamTag == false then
-                    v.ViewAngle.Visible = true
-                elseif Esp.Settings.TeamCheck == true and TeamTag == true then
-                    v.ViewAngle.Visible = false
-                else
-                    v.ViewAngle.Visible = true
-                end
-                if Esp.Settings.TargetSleepers == true and sleeping == true then
-                    v.ViewAngle.Visible = false
-                end
-                v.ViewAngle.Color = Esp.Settings.ViewAngleColor
-                v.ViewAngle.Thickness = Esp.Settings.ViewAngleThickness
-                v.Transparency = Esp.Settings.ViewAngleTransparrency
-                local headpos = Camera:WorldToViewportPoint(Character.Head.Position)
-                local offsetCFrame = CFrame.new(0, 0, -4)
-                v.ViewAngle.From = Vector2.new(headpos.X, headpos.Y)
-                local value = math.clamp(1 / Distance * 100, 0.1, 1)
-                local dir = Character.Head.CFrame:ToWorldSpace(offsetCFrame)
-                offsetCFrame = offsetCFrame * CFrame.new(0, 0, 0.4)
-                local dirpos = Camera:WorldToViewportPoint(Vector3.new(dir.X, dir.Y, dir.Z))
-                if OnScreen == true then
-                    v.ViewAngle.To = Vector2.new(dirpos.X, dirpos.Y)
-                    offsetCFrame = CFrame.new(0, 0, -4)
-                end
-            else
-                v.ViewAngle.Visible = false
+                drawings.ViewAngle.Visible = false
             end
         else
-            v.Box.Visible = false
-            v.Armour.Visible = false
-            v.Distance.Visible = false
-            v.ViewAngle.Visible = false
-            v.Sleeping.Visible = false
+            self:RemoveEsp(Player)
         end
     end
 end
 
-local PlayerUpdater = game:GetService("RunService").RenderStepped
-local PlayerConnection = PlayerUpdater:Connect(function()
+game:GetService("RunService").RenderStepped:Connect(function()
     Esp:UpdateEsp()
 end)
 
-for i, v in pairs(workspace:GetChildren()) do
-    if v:FindFirstChild("HumanoidRootPart") then
-        table.insert(cache, v)
-        Esp:CreateEsp({model = v})
+game:GetService("Workspace").ChildAdded:Connect(function(child)
+    if child:IsA("Model") and child:FindFirstChild("HumanoidRootPart") then
+        table.insert(cache, child)
+        Esp:CreateEsp(child)
+    end
+end)
+
+for _, child in ipairs(game:GetService("Workspace"):GetChildren()) do
+    if child:IsA("Model") and child:FindFirstChild("HumanoidRootPart") then
+        table.insert(cache, child)
+        Esp:CreateEsp(child)
     end
 end
 
-game:GetService("Workspace").ChildAdded:Connect(function(child)
-    if child:FindFirstChild("HumanoidRootPart") and not table.find(cache, child) then
-        table.insert(cache, child)
-        Esp:CreateEsp({model = child})
-    end
-end)
 
 local UserInputService = game:GetService("UserInputService")
 
